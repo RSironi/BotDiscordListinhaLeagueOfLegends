068798c61d6a0e43087a004b59e363330182df70
RSironi <96712987+RSironi@users.noreply.github.com>
2023-01-07 01:25:36 -0300

    Update listinha.py
    Agora as funções abrem e fecham conexão, corrigido erro em que servidor perdia conexão fazendo BOT não funcionar.

--- Script/listinha.py
+++ Script/listinha.py
@@ -1,25 +1,36 @@
-import connection as connection
-
+import connection
 #ADD - Command Bot = !!addListinha @User @Champ
 def add(userID: int, champName: str):
     sql = "INSERT INTO listinha (id_usuario,fk_champs_nome_champ) values (%s,%s);"
     val = (userID, champName)
+    connection.con.connect()
+    connection.cursor = connection.con.cursor() 
     connection.cursor.execute(sql,val)
     connection.con.commit()
+    connection.cursor.close()
+    connection.con.close()
 
 #DELETE - Command Bot = !!delListinha @User @Champ
 def delete(userID: int, champName: str):
     sql = "DELETE FROM listinha where id_usuario = %s and fk_champs_nome_champ = %s;"
     val = (userID,champName)
+    connection.con.connect()
+    connection.cursor= connection.con.cursor()
     connection.cursor.execute(sql,val)
     connection.con.commit()
+    connection.cursor.close()
+    connection.con.close()
 
 #SELECT - Command Bot = !!Listinha @User
 def select(userID: int, userName:str):
     result= f":\n:man_mage:SELADOS DE {userName}:man_mage:\n"
     sql = f"SELECT fk_champs_nome_champ from listinha where id_usuario = {userID};"
+    connection.con.connect()
+    connection.cursor= connection.con.cursor()
     connection.cursor.execute(sql)
     fetchresult = connection.cursor.fetchall()
     for x in fetchresult:
         result = result + ":no_entry_sign:" + x[0] + "\n"
+    connection.cursor.close()
+    connection.con.close()
     return result
